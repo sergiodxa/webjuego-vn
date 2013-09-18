5962d281aa4e44f0663f296c83a82c2efa45bb1f
sergiodxa <sergio@xalambri.com.ar>
2013-09-18 15:16:32 -0300

    sistema de guardado y cargado de partida+menú

--- js/juego.js
+++ js/juego.js
@@ -1,21 +1,42 @@
 //Funciones a ejecutar al cargar el sitio
 var escena = 0;
 $(document).on('ready', function(){
-	//$('#menu').hide();
+	$('#mensaje').hide();
+	$('#menu').hide();
 	$('#juego').css('background-image','url("img/bg/'+escenas[escena].bg+'")');
 	$('#dialogo').html(escenas[escena].dialogo);
 });
 
+/************************/
+/*	Funciones del menú	*/
+/************************/
 //Abrir menú
 function abrirMenu() {
 	$('#menu').fadeIn();
 };
-
 //Cerrar menú
 function cerrarMenu() {
 	$('#menu').fadeOut();
 };
-
+//Guardar partida
+function guardarPartida() {
+	localStorage.escena = escena;
+	$('#mensaje').html('Partida guardada con éxito').fadeIn();
+	setTimeout(function(){$('#mensaje').fadeOut();},1000);
+	cerrarMenu();
+}
+//Cargar partida
+function cargarPartida() {
+	escena = parseInt(localStorage.escena);
+	$('#juego').css('background-image','url("img/bg/'+escenas[escena].bg+'")');
+	$('#dialogo').html(escenas[escena].dialogo);
+	$('#mensaje').html('Partida cargada con éxito').fadeIn();
+	setTimeout(function(){$('#mensaje').fadeOut();},1000);
+	cerrarMenu();
+}
+/************************/
+/*	Funciones del juego	*/
+/************************/
 //Pasar a siguiente escena
 function siguiente() {
 	escena = escena+1;
