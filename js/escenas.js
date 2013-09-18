abdb683bf9e523fa641273ea11579740c47cd67e
sergiodxa <sergio@xalambri.com.ar>
2013-09-18 16:45:03 -0300

    funcionamiento por teclas y personajes agregados

--- js/escenas.js
+++ js/escenas.js
@@ -1,14 +1,23 @@
 var escenas =	[
 					{
 						'dialogo':'Hola y bienvenido a Webjuego VN, la Visual Novel creada utilizando HTML+CSS+JavaScript.',
-						'bg':'convenienceStore.jpg'
+						'bg':'convenienceStore.jpg',
+						'char1':'wikipetan.png',
+						'char2':'',
+						'char3':'',
 					},
 					{
 						'dialogo':'Como ves al hacer click en siguiente puedes pasar este nuevo diálogo, aunque el fondo se mantiene.',
-						'bg':'convenienceStore.jpg'
+						'bg':'convenienceStore.jpg',
+						'char1':'wikipetan.png',
+						'char2':'wikipetan.png',
+						'char3':'',
 					},
 					{
 						'dialogo':'Si haces click en el botón menú se despliega el menú del juego que da la opción de guardar o cargar la partida.',
-						'bg':'convenienceStore.jpg'
+						'bg':'convenienceStore.jpg',
+						'char1':'wikipetan.png',
+						'char2':'wikipetan.png',
+						'char3':'wikipetan.png',
 					}
 				];
--- js/juego.js
+++ js/juego.js
@@ -1,68 +1,99 @@
 //Funciones a ejecutar al cargar el sitio
-var escena = 0;
-$(document).on('ready', function(){
-	$('#mensaje').hide();
-	$('#menu').hide();
-	$('#juego').css('background-image','url("img/bg/'+escenas[escena].bg+'")');
-	$('#dialogo').html(escenas[escena].dialogo);
+
+var escena = 0; //definimos la escena inicial como 0
+
+$(document).on('ready', function(){ //ejecutamos el siguiente código inmediatamente que carga el sitio
+	$('#mensaje').hide(); //ocultamos el div #mensaje
+	$('#menu').hide(); //ocultamos el div #menu
+	cambiarEscena(); //cargamos la escena actual
 });
 
-/************************/
-/*	Funciones del menú	*/
-/************************/
 //Abrir menú
 function abrirMenu() {
-	$('#menu').fadeIn();
+	$('#menu').fadeIn(); //mostramos el menú
 };
-//Cerrar menú
-function cerrarMenu() {
-	$('#menu').fadeOut();
-};
-//Guardar partida
-function guardarPartida() {
-	localStorage.escena = escena;
-	$('#mensaje').html('Partida guardada con éxito').fadeIn();
-	setTimeout(function(){$('#mensaje').fadeOut();},1000);
-	cerrarMenu();
-}
-//Cargar partida
-function cargarPartida() {
-	escena = parseInt(localStorage.escena);
-	cambiarEscena();
-	$('#mensaje').html('Partida cargada con éxito').fadeIn();
-	setTimeout(function(){$('#mensaje').fadeOut();},1000);
-	cerrarMenu();
-}
-/************************/
-/*	Funciones del juego	*/
-/************************/
-//Pasar a siguiente escena
-function siguienteEscena() {
-	if (escena==escenas.length-1) {
-		$('#mensaje').html('No hay más escenas').fadeIn();
-		setTimeout(function(){$('#mensaje').fadeOut();},1000);
-	}
-	else {
-		++escena;
-		cambiarEscena();
-	}
-};
+
 //Volver a anterior escena
 function anteriorEscena() {
-	if (escena==0) {
-		$('#mensaje').html('No se puede ir más atras').fadeIn();
-		setTimeout(function(){$('#mensaje').fadeOut();},1000);
+	if (escena==0) { //comprobamos que escena sea igual 0
+		mostrarMensaje('No se puede ir más atras'); //si se cumple la condición mostramos un mensaje
 	}
 	else {
-		--escena;
-		cambiarEscena();
+		--escena; //si no se cumplió la condición a escena le restamos 1
+		cambiarEscena(); //llamamos a la función cambiar de escena
 	}
 };
+
 //Cambiar escena
 function cambiarEscena() {
-	$('#juego').css('background-image','url("img/bg/'+escenas[escena].bg+'")');
-	$('#dialogo').html(escenas[escena].dialogo);
-}
-/************/
-/*	Teclas	*/
-/************/
+	$('#juego').css('background-image','url("img/bg/'+escenas[escena].bg+'")'); //cambiamos el fondo por el de la escena actual
+	$('#dialogo').html(escenas[escena].dialogo); //cambiamos el diálogo por el de la escena actual
+	if (escenas[escena].char3 != '') {
+		$('#personajes').html('<img src="img/char/'+escenas[escena].char1+'" /><img src="img/char/'+escenas[escena].char2+'" /><img src="img/char/'+escenas[escena].char3+'" />'); //mostramos a los tres personajes
+	}
+	else if (escenas[escena].char2 != '') {
+		$('#personajes').html('<img src="img/char/'+escenas[escena].char1+'" /><img src="img/char/'+escenas[escena].char2+'" />'); //mostramos a los dos personajes
+	}
+	else if (escenas[escena].char1 != '') {
+		$('#personajes').html('<img src="img/char/'+escenas[escena].char1+'" />'); //mostramos al único personaje
+	}
+};
+
+//Cargar partida
+function cargarPartida() {
+	escena = parseInt(localStorage.escena); //cargamos a la variable escena el valor almacenado en localStorage
+	cambiarEscena(); //llamamos a la función cambiar de escena
+	mostrarMensaje('Partida cargada con éxito'); //llamamos a la función para mostrar el mensaje de que se cargó con éxito
+	cerrarMenu(); //cerramos el menú
+};
+
+//Cerrar menú
+function cerrarMenu() {
+	$('#menu').fadeOut(); //ocultamos el menú
+};
+
+//Guardar partida
+function guardarPartida() {
+	localStorage.escena = escena; //almacenamos en localStorage el número de escena actual
+	mostrarMensaje('Partida guardada con éxito'); //llamamos a la función para mostrar el mensaje de que se guardó con éxito
+	cerrarMenu(); //llamamos a la función cerrar el menú
+};
+
+//Mostrar mensaje
+function mostrarMensaje(mensaje) {
+	$('#mensaje').html(mensaje).fadeIn(); //mostramos el div #mensaje con el texto recibido
+	setTimeout(function(){$('#mensaje').fadeOut();},1000); //esperamos 1s y lo ocultamos
+};
+
+//Pasar a siguiente escena
+function siguienteEscena() {
+	if (escena==escenas.length-1) { //comprobamos que escena sea igual al valor más alto de escenas
+		mostrarMensaje('No hay más escenas'); //si se cumple la condición mostramos un mensaje
+	}
+	else {
+		++escena; //si no se cumplió la condición a escena le sumamos 1
+		cambiarEscena(); //llamamos a la función cambiar de escena
+	}
+};
+
+//Funciones ejecutadas al oprimir teclas
+$(document).on('keyup', function(evt) {
+	if (evt.keyCode=='13') { //tecla enter
+		siguienteEscena();
+	}
+	else if (evt.keyCode=='39') { //flecha a la derecha
+		siguienteEscena();
+	}
+	else if (evt.keyCode=='37') { //flecha a la izquierda
+		anteriorEscena();
+	}
+	else if (evt.keyCode=='27') { //tecla escape
+		abrirMenu();
+	}
+	else if (evt.keyCode=='83') { //tecla S
+		guardarPartida();
+	}
+	else if (evt.keyCode=='76') { //tecla L
+		cargarPartida();
+	}
+});
